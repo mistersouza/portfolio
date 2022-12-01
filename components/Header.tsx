f0aee9ea4ca59125fb5985f7b3a8610104a5f860
thiagosouza1983 <mistersouza@icloud.com>
2022-12-01 01:12:04 +0100

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header className='flex flex-row justify-between max-w-7xl mx-auto sticky top-0 z-10 xl-items-center p-5'>
+        <motion.div 
+            initial={{ x: -500, opacity: 0, scale: 0.5 }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5 }}
+            className='flex flex-row items-center'>
+            {/* Social Media icons */}
+            <SocialIcon url="https://www.linkedin.com/in/mistersouza/" fgColor='gray' bgColor='transparent'/>
+            <SocialIcon url="https://www.github.com/mistersouza/" fgColor='gray' bgColor='transparent'/>
+        </motion.div>
+        <motion.div 
+            initial={{ x: 500, opacity: 0, scale: 0.5 }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5 }}
+            className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            {/* Contact me */}
+            <SocialIcon
+                network='email'
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+        </motion.div>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
       <Head>
         <title>mistersouza's</title>
       </Head>
-
-      {/* Header */}
+      
+      <Header />
 
       {/* Hero */}
 
