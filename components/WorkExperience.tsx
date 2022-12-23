3a5dbae681fbb3d9e14cf9f2fe0e8c0fc404d082
thiagosouza1983 <mistersouza@icloud.com>
2022-12-23 20:15:21 +0100

    work experience

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+import WorkExperienceCard from './WorkExperienceCard'
+
 type Props = {}
 
 const WorkExperience = (props: Props) => {
@@ -11,12 +13,14 @@ const WorkExperience = (props: Props) => {
         transition={{ duration: 1.5 }}
         className='flex flex-col relative h-screen overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'   
     >
-        <h3 className='header'>Experience</h3>
+        <h3 className='absolute top-24 tracking-[20px] text-gray-500 text-2xl'>
+          Experience
+        </h3>
 
-        <div>
-            {/* WorkExperienceCard */}
-            {/* WorkExperienceCard */}
-            {/* WorkExperienceCard */}
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+          <WorkExperienceCard />
+          <WorkExperienceCard />
+          <WorkExperienceCard />
         </div>
     </motion.div>
   )
